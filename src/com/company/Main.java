420180b1200a9302585a94b02519ea16c9457b9d
Suslov Danil <danya.sus@yandex.ru>
2021-11-09 17:23:18 +0300

    init
    add Java default console files

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    public static int SquareOfExpression (int x) {
+        return x * x;
+    }
+
+    public static int CubeOfExpression (int x){
+        return x * x * x;
+    }
+}
